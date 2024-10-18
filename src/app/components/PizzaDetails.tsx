7b1391d8aa01fc363fb07f02319304815151b69e
Lucas Bastos <lucsbasto@gmail.com>
2024-10-17 22:37:53 -0300

    feat: toppings

--- src/app/components/PizzaDetails.tsx
+++ src/app/components/PizzaDetails.tsx
@@ -40,7 +40,7 @@ const PizzaDetails = ({ pizza, modal, setModal }: {pizza: Pizza, modal: boolean,
     } else {
       setAdditionalToppingPrice(0)
     }
-  })
+  }, [additionalTopping])
 
   return (
     <div className="flex flex-col lg:flex-row lg:gap-x-8 h-full md:p-8">
@@ -65,12 +65,12 @@ const PizzaDetails = ({ pizza, modal, setModal }: {pizza: Pizza, modal: boolean,
             </div>
             <SizeSelection pizza={pizza} size={size} setSize={setSize}/>
             <CrustSelection crust={crust} setCrust={setCrust}/>
-            <div>
+            <div className="mb-4 text-xl font-semibold">
               Choose topping
             </div>
-            <div>
+            <div className="flex flex-1 flex-wrap gap-2 py-1 justify-center lg:justify-start">
               {pizza.toppings.map((topping, index) => {
-                return <Topping key={index}/>
+                return <Topping key={index} topping={topping} additionalTopping={additionalTopping} setAdditionalTopping={setAdditionalTopping}/>
               })
               }
             </div>
--- src/app/components/Topping.tsx
+++ src/app/components/Topping.tsx
@@ -1,10 +1,46 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Image from "next/image"
+import { useEffect, useState } from "react"
+import { IoMdCheckmark } from "react-icons/io"
+import { Topping } from "../types"
 
-const Topping = () => {
+const ToppingComponent = ({ topping, additionalTopping, setAdditionalTopping }: {topping: Topping, additionalTopping: Topping[], setAdditionalTopping: any}) => {
+  const [isChecked, setIsChecked] = useState(false)
+
+  const handleCheckBox = () => {
+    setIsChecked(!isChecked)
+  }
+
+  const handleTopping = () => {
+    if(isChecked){
+      const newToppings = new Set([...additionalTopping, {...topping}])
+      setAdditionalTopping(Array.from(newToppings))
+    } else {
+      const newToppings = additionalTopping?.filter(toppingItem => {
+        return toppingItem.name !== topping.name
+      })
+      setAdditionalTopping([...newToppings])
+    }
+  }
+
+  useEffect(() => {
+    handleTopping()
+  }, [isChecked])
   return (
-    <div>
-      Topping
+    <div className={`
+      ${isChecked && 'border-orange'} 
+    w-full max-w-[110px] h-[140px] p-1 flex 
+    flex-col items-center justify-center border rounded-md bg-white relative`}>
+      <Image src={topping.image} width={70} height={70} alt='' priority={true} className="mb-2"/>
+      <div className="text-sm capitalize text-center font-medium">
+        {topping.name}  
+      </div>
+      <input className="absolute w-full h-full opacity-0 cursor-pointer" type="checkbox"  checked={isChecked} onChange={handleCheckBox}/>
+      <div className={`${isChecked ? 'opacity-100': 'opacity-0'} absolute top-1 right-1`}>
+        <IoMdCheckmark className="text-xl text-orange"/>
+      </div>
     </div>
   )
 }
 
-export default Topping
+export default ToppingComponent
