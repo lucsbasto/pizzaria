5921a608ec636773dc63d2d1b1728772e9901474
Lucas Bastos <lucsbasto@gmail.com>
2024-10-21 23:17:25 -0300

    fix: cart value

--- src/app/components/CartBottom.tsx
+++ src/app/components/CartBottom.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useContext } from "react"
 import { CartContext } from "../context/CartContext"
 
@@ -9,7 +10,9 @@ const CartBottom = () => {
         <div className="px-6 py-3 lg:py-6 mt-auto">
           <div className="flex items-center justify-between mb-6 text-lg font-semibold font-robotoCondensed">
             <div>Total:</div>
-            <div>R$ 320</div>
+            <div>R$ {cart.reduce((acc: any, curr: any) => {
+        return acc + curr.price
+      }, 0)}</div>
           </div>
           <div className="flex flex-col gap-y-3">
             <button className="btn btn-lg gradient font-semibold flex justify-center">Checkout</button>
@@ -17,7 +20,7 @@ const CartBottom = () => {
         </div>
       ) : (
         <div className="absolute top-0 w-full h-full flex justify-center items-center -z-10">
-          <div className="font-semibold">Your cart is empty</div>
+          <div className="font-semibold">Seu carrinho está vazio.</div>
         </div>
       )}
     </>
